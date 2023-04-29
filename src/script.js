81bad361713236649cb61f7815e510d9b3a4b593
Bruno Caroba <bruno_caroba@hotmail.com>
2023-04-29 11:07:02 -0300

    Corrigido erro no chrome com background-clip: text Adicionado modo para gerar bordar redondas ou quadradas Aumentado tamanho da fonte parar ficar com maior qualidade

--- src/script.js
+++ src/script.js
@@ -2,7 +2,18 @@ const BASE = document.querySelector("#content");
 const FORM = document.querySelector("#form");
 const TEMPLATE = BASE.children.item(0);
 
-const addTag = (title, color, text_color = "white", sImg = "", pImg = "") => {
+const _serializeToString = XMLSerializer.prototype.serializeToString;
+
+XMLSerializer.prototype.serializeToString = function (node) {
+    return _serializeToString
+        .call(this, node)
+        .replace(
+            /background-image:/g,
+            '-webkit-background-clip: text; background-image:',  // Add the -webkit-background-clip
+        );
+};
+
+const addTag = (title, color, text_color = "white", sImg = "", pImg = "", borderSquared = false) => {
 
     let copy = TEMPLATE.cloneNode(true);
 
@@ -15,7 +26,7 @@ const addTag = (title, color, text_color = "white", sImg = "", pImg = "") => {
         .replaceAll("<img class=\"tag-img\" src=\"\" alt=\"\">", "")
         .replaceAll("<img class=\"tag-img tag-img-suffix\" src=\"\" alt=\"\">", "");
 
-    copy.style = `--tag-background: ${color}; --text-color: ${text_color};`
+    copy.style = `--tag-background: ${color}; --text-color: ${text_color}; --border-radius: ${borderSquared ? 0 : "5px"}`
 
     return copy;
 }
@@ -35,22 +46,22 @@ FORM.addEventListener("submit", evt => {
     const border = document.querySelector("#border").value
     const pImg = document.querySelector("#p-img").value
     const sImg = document.querySelector("#s-img").value
-
+    const borderSquared = document.querySelector("#border-squared").checked
 
     BASE.innerHTML = "";
 
     if(cor === "#000000"){
-        gerarRandom(title, border, cor, sImg, pImg)
+        gerarRandom(title, border, cor, sImg, pImg, borderSquared)
     }else {
-        gerar(title, border, cor, sImg, pImg)
+        gerar(title, border, cor, sImg, pImg,borderSquared)
     }
 
 })
 
-const gerar = (title, border, cor, sImg, pImg) => {
-    BASE.appendChild(addTag(title, cor, border, sImg, pImg))
+const gerar = (title, border, cor, sImg, pImg, borderSquared) => {
+    BASE.appendChild(addTag(title, cor, border, sImg, pImg, borderSquared))
 }
-const gerarRandom = (title, cor, sImg, pImg) => {
+const gerarRandom = (title, cor, sImg, pImg, borderSquared) => {
 
     let data = [];
 
@@ -59,7 +70,7 @@ const gerarRandom = (title, cor, sImg, pImg) => {
         let arr = [];
 
         for(let j = 0; j < 100; j += 10){
-            arr.push(addTag(title, `hsl(${i},100%,${j}%)`, cor, sImg, pImg))
+            arr.push(addTag(title, `hsl(${i},100%,${j}%)`, cor, sImg, pImg, borderSquared))
         }
 
         data.push(arr)
